18c7ec72333a64812ad31ec6305f9a5cd6aefe03
Aaron Lippold <lippold@gmail.com>
2025-01-24 02:38:05 -0500

    ran chefstyle
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- lib/inspec/resources/sudoers.rb
+++ lib/inspec/resources/sudoers.rb
@@ -8,7 +8,7 @@ module Inspec::Resources
     filter.register_column(:run_as, field: :run_as)
     filter.register_column(:tags, field: :tags)
     filter.register_column(:commands, field: :commands)
-    filter.register_custom_matcher(:nopasswd?) { |x| x.tags && x.tags.include?('NOPASSWD:') }
+    filter.register_custom_matcher(:nopasswd?) { |x| x.tags && x.tags.include?("NOPASSWD:") }
     filter.install_filter_methods_on_resource(self, :table)
 
     attr_reader :table
@@ -18,7 +18,7 @@ module Inspec::Resources
     end
 
     def to_s
-      'Sudoers Rules'
+      "Sudoers Rules"
     end
   end
 
@@ -37,19 +37,19 @@ module Inspec::Resources
     end
 
     def to_s
-      'Sudoers Settings'
+      "Sudoers Settings"
     end
   end
 
   # This class represents the sudoers resource in InSpec, used to test sudo configuration.
   class Sudoers < Inspec.resource(1)
-    name 'sudoers'
-    supports platform: 'unix'
-    supports platform: 'darwin'
-    supports platform: 'freebsd'
-    supports platform: 'solaris'
-    supports platform: 'aix'
-    desc 'Use the sudoers InSpec audit resource to test the configuration of sudo.'
+    name "sudoers"
+    supports platform: "unix"
+    supports platform: "darwin"
+    supports platform: "freebsd"
+    supports platform: "solaris"
+    supports platform: "aix"
+    desc "Use the sudoers InSpec audit resource to test the configuration of sudo."
     example <<~EXAMPLE
       # Test that there are no NOPASSWD rules
       describe sudoers.rules.where { !tags.nil? && tags.include?('NOPASSWD:') } do
@@ -66,7 +66,10 @@ module Inspec::Resources
 
     # List of directives to be ignored during parsing
     # Add any new directives that should be ignored to this list
-    IGNORED_DIRECTIVES = ['#includedir'].freeze
+    # consider adding a test to ensure the directive is ignored
+    # consider adding a warning if the directive is not ignored
+    # TODO: add a way for the user to pass a directive to ignore to the resource & add tests
+    IGNORED_DIRECTIVES = ["#includedir"].freeze
 
     def initialize(sudoers_files = nil)
       super()
@@ -74,8 +77,8 @@ module Inspec::Resources
       Inspec::Log.debug("sudoers_files: #{@sudoers_files}")
 
       if @sudoers_files.empty?
-        Inspec::Log.warn('No sudoers files found. Skipping resource.')
-        skip_resource 'No sudoers files found.'
+        Inspec::Log.warn("No sudoers files found. Skipping resource.")
+        skip_resource "No sudoers files found."
       end
 
       load_content
@@ -91,16 +94,16 @@ module Inspec::Resources
     end
 
     def to_s
-      "Sudoers Configuration #{@sudoers_files.join(', ')}"
+      "Sudoers Configuration #{@sudoers_files.join(", ")}"
     end
 
     # Helper methods
     def authenticate?
-      settings.Defaults.include?('!authenticate')
+      settings.Defaults.include?("!authenticate")
     end
 
     def timeout_value
-      timeout = settings.Defaults['timestamp_timeout']
+      timeout = settings.Defaults["timestamp_timeout"]
       timeout ? timeout.first.to_i : nil
     end
 
@@ -109,15 +112,15 @@ module Inspec::Resources
     end
 
     def user_aliases
-      settings.where(name: 'User_Alias')
+      settings.where(name: "User_Alias")
     end
 
     def command_aliases
-      settings.where(name: 'Cmnd_Alias')
+      settings.where(name: "Cmnd_Alias")
     end
 
     def defaults
-      settings.where(name: 'Defaults')
+      settings.where(name: "Defaults")
     end
 
     def user_defaults(user)
@@ -128,16 +131,16 @@ module Inspec::Resources
 
     def default_sudoers_path
       path = case inspec.os.name
-             when 'darwin'
-               '/private/etc/sudoers'
-             when 'freebsd'
-               '/usr/local/etc/sudoers'
-             when 'solaris'
-               '/etc/opt/sudoers'
-             when 'aix'
-               '/etc/security/sudoers'
+             when "darwin"
+               "/private/etc/sudoers"
+             when "freebsd"
+               "/usr/local/etc/sudoers"
+             when "solaris"
+               "/etc/opt/sudoers"
+             when "aix"
+               "/etc/security/sudoers"
              else
-               '/etc/sudoers'
+               "/etc/sudoers"
              end
 
       Inspec::Log.warn("Default sudoers path #{path} does not exist.") unless inspec.file(path).exist?
@@ -146,22 +149,22 @@ module Inspec::Resources
     end
 
     def load_content
-      @raw_content = inspec.command("cat #{@sudoers_files.join(' ')}").stdout
+      @raw_content = inspec.command("cat #{@sudoers_files.join(" ")}").stdout
       if @raw_content.empty?
-        Inspec::Log.warn("Failed to load content from sudoers files: #{@sudoers_files.join(', ')}")
-        skip_resource 'Failed to load content from sudoers files.'
+        Inspec::Log.warn("Failed to load content from sudoers files: #{@sudoers_files.join(", ")}")
+        skip_resource "Failed to load content from sudoers files."
       end
       Inspec::Log.debug("raw_content: #{@raw_content}")
       @lines = @raw_content.lines.reject do |line|
-                 line.nil? || line.match(/^#(?!include)|^\s*$/) || IGNORED_DIRECTIVES.any? do |directive|
-                   line.include?(directive)
-                 end
+        line.nil? || line.match(/^#(?!include)|^\s*$/) || IGNORED_DIRECTIVES.any? do |directive|
+          line.include?(directive)
+        end
       end.map(&:strip)
       Inspec::Log.debug("lines: #{@lines}")
     end
 
     def parse_content
-      aliases = %w[Defaults Cmnd_Alias User_Alias Host_Alias Runas_Alias]
+      aliases = %w{Defaults Cmnd_Alias User_Alias Host_Alias Runas_Alias}
       settings_lines = @lines.select { |line| line.match(/^(#{aliases.join('|')})/) }
       userspec_lines = @lines.reject { |line| line.match(/^(#{aliases.join('|')})/) }
       Inspec::Log.debug("settings_lines: #{settings_lines}")
@@ -176,12 +179,12 @@ module Inspec::Resources
     def settings_hash(settings_lines)
       parse_options = {
         assignment_regex: /^\s*([^=]*?)\s*\+?=\s*(.*?)\s*$/,
-        multiple_values: true
+        multiple_values: true,
       }
       sudo_config_data = inspec.parse_config(settings_lines.join("\n"), parse_options).params
       sudo_config_hash = Hashie::Mash.new
       sudo_config_data.each do |k, v|
-        if k.start_with?('Defaults')
+        if k.start_with?("Defaults")
           key_parts = k.split("\s", 2)
           sudo_config_hash.Defaults ||= Hashie::Mash.new
           sudo_config_hash.Defaults[key_parts[1].strip] = v.map { |x| x.delete('"').split(/,\s*/) }.flatten
@@ -199,19 +202,19 @@ end
 # This class is responsible for parsing and filtering user specifications in the sudoers file.
 class SudoersUserSpecTable
   FilterTable.create
-             .register_column(:users, field: :users)
-             .register_column(:hosts, field: :hosts)
-             .register_column(:run_as, field: :run_as)
-             .register_column(:tags, field: :tags)
-             .register_column(:commands, field: :commands)
-             .install_filter_methods_on_resource(self, :table)
+    .register_column(:users, field: :users)
+    .register_column(:hosts, field: :hosts)
+    .register_column(:run_as, field: :run_as)
+    .register_column(:tags, field: :tags)
+    .register_column(:commands, field: :commands)
+    .install_filter_methods_on_resource(self, :table)
 
   attr_reader :table
 
   def initialize(userspec_lines)
     Inspec::Log.debug("Initializing SudoersUserSpecTable with userspec_lines: #{userspec_lines}")
-    tags = %w[NOPASSWD PASSWD NOEXEC EXEC SETENV NOSETENV LOG_INPUT NOLOG_INPUT LOG_OUTPUT
-              NOLOG_OUTPUT]
+    tags = %w{NOPASSWD PASSWD NOEXEC EXEC SETENV NOSETENV LOG_INPUT NOLOG_INPUT LOG_OUTPUT
+              NOLOG_OUTPUT}
     @table = userspec_lines.map.with_index do |line, index|
       line_hash = {}
       parsed_line = line.match(/^(?<users>\S+)\s+(?<hosts>[^\s=]+)\s*=\s*(\((?<run_as>[^)]+)\))?\s*(?<tags>(#{tags.join(':|')}:)+)?\s*(?<commands>.*)$/)
@@ -220,12 +223,12 @@ class SudoersUserSpecTable
         Inspec::Log.warn("Unable to parse line #{index + 1}: #{line}")
       else
         Inspec::Log.debug("parsed_line: #{parsed_line}")
-        line_hash[:users] = parsed_line['users']
-        line_hash[:hosts] = parsed_line['hosts']
-        line_hash[:run_as] = parsed_line['run_as'] unless parsed_line['run_as'].nil?
-        line_hash[:tags] = parsed_line['tags'] unless parsed_line['tags'].nil?
-        line_hash[:commands] = parsed_line['commands']
-        line_hash.transform_values! { |v| !v.nil? && v.include?(',') ? v.split(',') : v }
+        line_hash[:users] = parsed_line["users"]
+        line_hash[:hosts] = parsed_line["hosts"]
+        line_hash[:run_as] = parsed_line["run_as"] unless parsed_line["run_as"].nil?
+        line_hash[:tags] = parsed_line["tags"] unless parsed_line["tags"].nil?
+        line_hash[:commands] = parsed_line["commands"]
+        line_hash.transform_values! { |v| !v.nil? && v.include?(",") ? v.split(",") : v }
       end
       line_hash unless line_hash.empty?
     end.compact
@@ -233,6 +236,6 @@ class SudoersUserSpecTable
   end
 
   def to_s
-    'Sudoers User Permissions Table'
+    "Sudoers User Permissions Table"
   end
 end
--- test/unit/resources/sudoers_test.rb
+++ test/unit/resources/sudoers_test.rb
@@ -1,189 +1,189 @@
-require 'helper'
-require 'inspec/resource'
-require 'inspec/resources/sudoers'
+require "helper"
+require "inspec/resource"
+require "inspec/resources/sudoers"
 
-describe 'Inspec::Resources::Sudoers' do
-  describe 'properties and interface' do
-    resource = load_resource('sudoers')
+describe "Inspec::Resources::Sudoers" do
+  describe "properties and interface" do
+    resource = load_resource("sudoers")
 
-    it 'verifies resource_id is /etc/sudoers by default' do
-      _(resource.resource_id).must_equal '/etc/sudoers'
+    it "verifies resource_id is /etc/sudoers by default" do
+      _(resource.resource_id).must_equal "/etc/sudoers"
     end
 
-    it 'verifies raw_content is loaded' do
+    it "verifies raw_content is loaded" do
       _(resource.raw_content).wont_be_nil
-      _(resource.raw_content).must_include 'Defaults'
+      _(resource.raw_content).must_include "Defaults"
     end
   end
 
-  describe 'Default settings parsing' do
-    let(:resource) { load_resource('sudoers') }
+  describe "Default settings parsing" do
+    let(:resource) { load_resource("sudoers") }
 
-    it 'parses authentication flags correctly' do
+    it "parses authentication flags correctly" do
       settings = resource.settings.Defaults
-      _(settings).must_include '!authenticate'
-      _(settings).must_include '!targetpw'
-      _(settings).must_include '!rootpw'
-      _(settings).must_include '!runaspw'
+      _(settings).must_include "!authenticate"
+      _(settings).must_include "!targetpw"
+      _(settings).must_include "!rootpw"
+      _(settings).must_include "!runaspw"
     end
 
-    it 'parses timeout settings correctly' do
-      timeout = resource.settings.Defaults['timestamp_timeout']
+    it "parses timeout settings correctly" do
+      timeout = resource.settings.Defaults["timestamp_timeout"]
       _(timeout).wont_be_nil
       _(timeout.first.to_i).must_equal 0
     end
 
-    it 'properly parses all user aliases' do
+    it "properly parses all user aliases" do
       user_aliases = resource.settings.User_Alias
       _(user_aliases).must_be_kind_of Hash
-      _(user_aliases['ADMINS']).must_include 'admin'
-      _(user_aliases['ADMINS']).must_include 'wheel'
+      _(user_aliases["ADMINS"]).must_include "admin"
+      _(user_aliases["ADMINS"]).must_include "wheel"
     end
 
-    it 'properly parses all command aliases' do
+    it "properly parses all command aliases" do
       cmd_aliases = resource.settings.Cmnd_Alias
       _(cmd_aliases).must_be_kind_of Hash
-      _(cmd_aliases['SOFTWARE']).must_include '/bin/rpm'
-      _(cmd_aliases['SERVICES']).must_include '/usr/sbin/service'
+      _(cmd_aliases["SOFTWARE"]).must_include "/bin/rpm"
+      _(cmd_aliases["SERVICES"]).must_include "/usr/sbin/service"
     end
 
-    it 'handles multiple Defaults lines' do
+    it "handles multiple Defaults lines" do
       defaults = resource.settings.Defaults
-      _(defaults.keys).must_include 'env_reset'
-      _(defaults.keys).must_include 'mail_badpass'
+      _(defaults.keys).must_include "env_reset"
+      _(defaults.keys).must_include "mail_badpass"
     end
 
-    it 'handles user-specific Defaults' do
-      defaults = resource.settings['Defaults:root']
+    it "handles user-specific Defaults" do
+      defaults = resource.settings["Defaults:root"]
       _(defaults).wont_be_nil if defaults
     end
 
-    it 'handles Defaults with multiple values' do
-      env_keep = resource.settings.Defaults['env_keep']
+    it "handles Defaults with multiple values" do
+      env_keep = resource.settings.Defaults["env_keep"]
       _(env_keep).must_be_kind_of Array
-      _(env_keep).must_include 'HOME'
+      _(env_keep).must_include "HOME"
     end
   end
 
-  describe 'Alias parsing' do
-    let(:resource) { load_resource('sudoers') }
+  describe "Alias parsing" do
+    let(:resource) { load_resource("sudoers") }
 
-    it 'handles command alias expansion' do
-      cmnd = resource.settings.Cmnd_Alias['SERVICES']
-      _(cmnd).must_include '/usr/sbin/service'
-      _(cmnd).must_include '/usr/bin/systemctl'
+    it "handles command alias expansion" do
+      cmnd = resource.settings.Cmnd_Alias["SERVICES"]
+      _(cmnd).must_include "/usr/sbin/service"
+      _(cmnd).must_include "/usr/bin/systemctl"
     end
 
-    it 'handles user alias expansion' do
-      users = resource.settings.User_Alias['ADMINS']
-      _(users).must_include 'admin'
-      _(users).must_include 'wheel'
+    it "handles user alias expansion" do
+      users = resource.settings.User_Alias["ADMINS"]
+      _(users).must_include "admin"
+      _(users).must_include "wheel"
     end
 
-    it 'handles host alias expansion' do
-      hosts = resource.settings.Host_Alias['WEBSERVERS']
-      _(hosts).must_include 'www1'
-      _(hosts).must_include 'www2'
+    it "handles host alias expansion" do
+      hosts = resource.settings.Host_Alias["WEBSERVERS"]
+      _(hosts).must_include "www1"
+      _(hosts).must_include "www2"
     end
   end
 
-  describe 'User specifications parsing' do
-    resource = load_resource('sudoers')
+  describe "User specifications parsing" do
+    resource = load_resource("sudoers")
 
-    it 'parses user rules correctly' do
-      user_rules = resource.rules.where { users == 'root' }
+    it "parses user rules correctly" do
+      user_rules = resource.rules.where { users == "root" }
       _(user_rules.entries).wont_be_empty
-      _(user_rules.entries.first[:hosts]).must_equal 'ALL'
-      _(user_rules.entries.first[:commands]).must_equal 'ALL'
+      _(user_rules.entries.first[:hosts]).must_equal "ALL"
+      _(user_rules.entries.first[:commands]).must_equal "ALL"
     end
 
-    it 'parses host specifications correctly' do
-      host_rules = resource.rules.where { hosts == 'WEBSERVERS' }
+    it "parses host specifications correctly" do
+      host_rules = resource.rules.where { hosts == "WEBSERVERS" }
       _(host_rules.entries).wont_be_empty
-      _(host_rules.entries.first[:users]).must_equal 'www-data'
-      _(host_rules.entries.first[:commands]).must_equal '/usr/sbin/nginx'
+      _(host_rules.entries.first[:users]).must_equal "www-data"
+      _(host_rules.entries.first[:commands]).must_equal "/usr/sbin/nginx"
     end
 
-    it 'handles NOPASSWD tags' do
-      nopasswd_rules = resource.rules.where { !tags.nil? && tags.include?('NOPASSWD:') }
+    it "handles NOPASSWD tags" do
+      nopasswd_rules = resource.rules.where { !tags.nil? && tags.include?("NOPASSWD:") }
       _(nopasswd_rules.entries).wont_be_empty
-      _(nopasswd_rules.entries.first[:users]).must_equal 'admin'
+      _(nopasswd_rules.entries.first[:users]).must_equal "admin"
     end
 
-    it 'correctly parses run_as specifications' do
+    it "correctly parses run_as specifications" do
       run_as_rules = resource.rules.where { !run_as.nil? }
       _(run_as_rules.entries).wont_be_empty
-      _(run_as_rules.entries.first[:run_as]).must_include 'ALL'
+      _(run_as_rules.entries.first[:run_as]).must_include "ALL"
     end
   end
 
-  describe 'Multiple file handling' do
-    resource = load_resource('sudoers', ['/etc/sudoers', '/etc/sudoers.d/*'])
+  describe "Multiple file handling" do
+    resource = load_resource("sudoers", ["/etc/sudoers", "/etc/sudoers.d/*"])
 
-    it 'loads content from multiple files' do
+    it "loads content from multiple files" do
       _(resource.raw_content).wont_be_empty
       _(resource.sudoers_files.count).must_equal 2
     end
   end
 
-  describe 'Platform-specific handling' do
-    it 'supports macOS (Darwin)' do
-      resource = load_resource('sudoers', '/private/etc/sudoers')
-      _(resource.resource_id).must_equal '/private/etc/sudoers'
+  describe "Platform-specific handling" do
+    it "supports macOS (Darwin)" do
+      resource = load_resource("sudoers", "/private/etc/sudoers")
+      _(resource.resource_id).must_equal "/private/etc/sudoers"
       _(resource.raw_content).wont_be_nil
     end
 
-    it 'supports FreeBSD' do
-      resource = load_resource('sudoers', '/usr/local/etc/sudoers')
-      _(resource.resource_id).must_equal '/usr/local/etc/sudoers'
+    it "supports FreeBSD" do
+      resource = load_resource("sudoers", "/usr/local/etc/sudoers")
+      _(resource.resource_id).must_equal "/usr/local/etc/sudoers"
       _(resource.raw_content).wont_be_nil
     end
 
-    it 'supports Solaris' do
-      resource = load_resource('sudoers', '/etc/opt/sudoers')
-      _(resource.resource_id).must_equal '/etc/opt/sudoers'
+    it "supports Solaris" do
+      resource = load_resource("sudoers", "/etc/opt/sudoers")
+      _(resource.resource_id).must_equal "/etc/opt/sudoers"
       _(resource.raw_content).wont_be_nil
     end
 
-    it 'supports AIX' do
-      resource = load_resource('sudoers', '/etc/security/sudoers')
-      _(resource.resource_id).must_equal '/etc/security/sudoers'
+    it "supports AIX" do
+      resource = load_resource("sudoers", "/etc/security/sudoers")
+      _(resource.resource_id).must_equal "/etc/security/sudoers"
       _(resource.raw_content).wont_be_nil
     end
   end
 
-  describe 'Helper methods' do
-    let(:resource) { load_resource('sudoers') }
+  describe "Helper methods" do
+    let(:resource) { load_resource("sudoers") }
 
-    it 'checks if authentication is required' do
+    it "checks if authentication is required" do
       _(resource.authenticate?).must_equal true
     end
 
-    it 'returns the timeout value' do
+    it "returns the timeout value" do
       _(resource.timeout_value).must_equal 0
     end
 
-    it 'checks if a timeout value is set' do
+    it "checks if a timeout value is set" do
       _(resource.timeout_value?).must_equal true
     end
 
-    it 'returns user aliases' do
+    it "returns user aliases" do
       user_aliases = resource.user_aliases
       _(user_aliases.entries).wont_be_empty if user_aliases
     end
 
-    it 'returns command aliases' do
+    it "returns command aliases" do
       command_aliases = resource.command_aliases
       _(command_aliases.entries).wont_be_empty if command_aliases
     end
 
-    it 'returns default settings' do
+    it "returns default settings" do
       defaults = resource.defaults
       _(defaults.entries).wont_be_empty if defaults
     end
 
-    it 'returns user-specific defaults' do
-      user_defaults = resource.user_defaults('root')
+    it "returns user-specific defaults" do
+      user_defaults = resource.user_defaults("root")
       _(user_defaults.entries).wont_be_empty if user_defaults
     end
   end
